714099dc10902bce069127afeeb9d7621c48173b
Blu <304entity304@gmail.com>
2026-02-26 11:43:51 +0100

    svg z1

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-
+        Point point = new Point();
+        point.x = 5.6F;
+        point.y = 2.4F;
+        System.out.println(point);
     }
 }
---
+++ svg/src/Point.java
@@ -0,0 +1,12 @@
+public class Point {
+    public float x;
+    public double y;
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
